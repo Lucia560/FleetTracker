8f076e2223587b3edc902e8613cf8fd7065a53f3
Lucia Predescu <luciana-579@hotmail.com>
2025-01-17 19:43:11 +0100

    Initial Commit

---
+++ src/test/java/com/example/FleetTrack/FleetTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FleetTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
